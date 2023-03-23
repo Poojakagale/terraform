743527a6e0a6895804e3a0750ab03a26256319bb
Poojakagale <poojankagale1996@gmail.com>
2023-03-23 16:16:19 +0530

    test

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73"
+    }
+  }
+}
+provider "aws"{
+    region = "us-east-1"
+    profile = "awsprofile"
+}
+resource "aws_instance" "ec2_1"{
+    ami = "ami-0557a15b87f6559cf"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform_ec2"
+    }
+}
